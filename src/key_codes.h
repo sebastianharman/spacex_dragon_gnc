8e64d50f84f346b22ffdd0ea0f2c43ffa71837c3
Sebastian Harman <sebastianharman1@gmail.org>
2021-05-09 22:23:32 +1000

    its working!

---
+++ src/key_codes.h
@@ -0,0 +1,18 @@
+#ifndef KEY_CODES_H
+#define KEY_CODES_H
+
+// windows virtual key codes
+#define FULLSTOP_KEY        0xBE    // roll right
+#define COMMA_KEY           0xBC    // roll left
+#define LEFT_ARROW          0x25    // yaw left
+#define UP_ARROW            0x26    // pitch up
+#define RIGHT_ARROW         0x27    // yaw right
+#define DOWN_ARROW          0x28    // pitch down
+#define W_KEY               0x57    // pos up
+#define A_KEY               0x41    // pos left
+#define S_KEY               0x53    // pos down
+#define D_KEY               0x44    // pos right
+#define E_KEY               0x45    // pos forwards
+#define Q_KEY               0x51    // pos backwards
+
+#endif
